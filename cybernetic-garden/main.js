8a4cbf1c8c21a54492c827e6d83615a55e8953fe
mahira <42784156+mahirarai@users.noreply.github.com>
2021-02-08 12:04:45 -0500

    cybernetic garden added

---
+++ cybernetic-garden/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+
+    console.log("page loaded");
+
+    var backgrounds = [
+        "url(images/beachhouse.gif)",
+        "url(images/scan1.jpg)",
+        "url(images/nightlight.jpg)",
+        "url(images/swirl.gif)",
+        "url(images/meadow_dark.jpg)"];
+    var index = 0;
+
+    var words = [
+        "i like to think",
+        "i like to feel",
+        "i like to move",
+        "i like to dream",
+        "i like to rest",
+        "i like to play",
+        "i like to cry",
+        "i like to listen",
+        "i like to breathe"
+    ]
+    //    var randWord = Math.floor(Math.random()*words.length);
+    //    $('#text-main').text(words[randWord]);
+
+    var secondWords = [
+        "and think.",
+        "and feel.",
+        "and move.",
+        "and dream.",
+        "and rest.",
+        "and play.",
+        "and cry.",
+        "and listen.",
+        "and breathe."
+    ]
+    var randWordSecond = Math.floor(Math.random() * secondWords.length);
+    $('#text-second').text(secondWords[randWordSecond]);
+
+    $('.button2').click(function () {
+        var randWordSecond = Math.floor(Math.random() * secondWords.length);
+        $('#text-second').text(secondWords[randWordSecond]);
+    })
+
+    $('.button').click(function () {
+        console.log("button clicked");
+
+        $("body").css('background-image', backgrounds[index]);
+        console.log(index);
+        console.log(backgrounds[index]);
+
+        // change word
+        var randWord = Math.floor(Math.random() * words.length);
+        $('#text-main').text(words[randWord]);
+
+        // update index of background
+        index++;
+        if (index == backgrounds.length) {
+            index = 0;
+        }
+    })
+
+
+    $(function () {
+        $('#text-main').draggable();
+        $('#text-second').draggable();
+        $('.circle').draggable();
+        $('.circle-red').draggable();
+        $('.square').draggable();
+        $('.square-red').draggable();
+        $('.triangle-up').draggable();
+        $('.triangle-down').draggable();
+        $('.triangle-left').draggable();
+        $('.triangle-right').draggable();
+    });
+
+});
